db6856e6fb5b814fd31dab6a0cb308ba90f93732
Sam Rae <samuelrae@gmail.com>
2015-08-13 19:45:18 +0100

    got trip calculator working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 console.log('hello, lauren');
+
+//BASIC CALC
 var answer;
 // 1. write an event listener on the basic-calc button
 
@@ -7,14 +9,20 @@ var basicButton = document.getElementById('basic-calc');
 basicButton.addEventListener ('click',function() {
   console.log('click event fired');
   BasicCalc();
+  showBasicAnswer();
   }
 );
 
-// 2. inside the function on that event listener collect the values from the user and store them in variables
+function showBasicAnswer() {
+   document.getElementById('basic-answer-alert').innerHTML=answer;
+  document.getElementById('basic-answer').className='show';
+};
 
-// 3. do the relevant calculations on those variables
-// 4. show the answer div
-// 5. update the text of the answer div accordingly
+// 2. inside the function on that event listener collect the values from the user and store them in variables DONE
+
+// 3. do the relevant calculations on those variables DONE
+// 4. show the answer div DONE
+// 5. update the text of the answer div accordingly DONE
 
 function BasicCalc() {
 
@@ -36,15 +44,38 @@ var operator = document.getElementById("basic-operation").value;
         answer = first * second;
     };
     // display the result
-answer=answer.toFixed(3);
-returnAnswer();
-showAnswerBox();
+answer=answer.toFixed(2);
 }
 
-function showAnswerBox() {
-  document.getElementById('basic-answer').className='show';
+//TRIP
+
+var tripButton = document.getElementById('trip-calc');
+
+tripButton.addEventListener ('click',function() {
+  console.log('click event fired');
+  trip();
+  showTripAnswer();
+  }
+);
+
+function showTripAnswer() {
+   document.getElementById('trip-answer-alert').innerHTML=trip();
+  document.getElementById('trip-answer').className='show';
 };
 
-function returnAnswer() {
-  document.getElementById('basic-answer-alert').innerHTML=answer;
-};
+function trip() {
+var dist = document.getElementById('trip-distance').value;
+var mpg = document.getElementById('trip-mpg').value;
+var cost = document.getElementById('trip-cost').value;
+var speed  = document.getElementById('trip-speed').value;
+
+var time = dist / speed;
+time = Math.round(time * 100) / 100; // round it to two decimals
+var actualMPG = (mpg > 60) ? Math.max(0, mpg - (speed - 60) * 2) : mpg;
+
+var cost = (dist / actualMPG * cost);
+cost = Math.round(cost * 100) / 100;
+
+// finally, display the result
+return("Your trip will take " + time + " hours and cost $" + cost);
+}
