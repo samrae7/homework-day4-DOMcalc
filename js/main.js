ac3616d96fef28d2ebcb9e924b5b71b916bd4b83
Sam Rae <samuelrae@gmail.com>
2015-08-13 18:19:31 +0100

    got basic calculator working with pop-up alert

--- js/main.js
+++ js/main.js
@@ -2,13 +2,11 @@ console.log('hello, lauren');
 
 // 1. write an event listener on the basic-calc button
 
-var first;
-
 var basicButton = document.getElementById('basic-calc');
 
 basicButton.addEventListener ('click',function() {
-  first  =  document.getElementById("basic-num-1").value;
-  console.log(first);
+  console.log('click event fired');
+  alert(BasicCalc());
   }
 );
 
@@ -18,30 +16,25 @@ basicButton.addEventListener ('click',function() {
 // 4. show the answer div
 // 5. update the text of the answer div accordingly
 
-  // Ask the user to enter the first number
-  
+function BasicCalc() {
 
-    // var operator = prompt("What do you want to do? (p)lus / (s)ubtract / (d)ivide / (m)ultiply");
+var first = parseFloat(document.getElementById("basic-num-1").value);
+var second  = parseFloat(document.getElementById("basic-num-2").value);
+var operator = document.getElementById("basic-operation").value; 
 
- 
-
-    // Ask the user to enter the second number
-    // var second =  parseInt(prompt("What is your second number?"));
-
-    // Calculate the answer based on the chosen basic operator
-    // switch(operator){
-    //   case "p":
-    //     answer = first + second;
-    //     break;
-    //   case "s":
-    //     answer = first - second;
-    //     break;
-    //   case "d":
-    //     answer = first / second;
-    //     break;
-    //   case "m":
-    //     answer = first * second;
-    // //     break;
-    // // };
-    // // finally, display the result
-    // alert('the answer is ' + answer);
+    switch(operator){
+      case "+":
+        answer = first + second;
+        break;
+      case "-":
+        answer = first - second;
+        break;
+      case "/":
+        answer = first / second;
+        break;
+      case "*":
+        answer = first * second;
+    };
+    // display the result
+return (answer);
+}
